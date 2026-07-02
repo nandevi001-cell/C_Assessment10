beda30b80a1becee546c43f2eb657027ad9bac26
nandevi001-cell <nandevi001@gmail.com>
2026-07-02 19:27:23 +0530

    Create C_program_5.c

---
+++ C_program_5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, i = 0, j;
+    char str[100], temp;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while(num > 0)
+    {
+        str[i] = (num % 10) + 48;
+        num = num / 10;
+        i++;
+    }
+
+    str[i] = '\0';
+
+    for(j = 0; j < i/2; j++)
+    {
+        temp = str[j];
+        str[j] = str[i-j-1];
+        str[i-j-1] = temp;
+    }
+
+    printf("String = %s", str);
+}
