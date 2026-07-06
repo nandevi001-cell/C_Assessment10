dbe9213d748a5960fe8cbd4d89f835860ca5bc2c
nandevi001-cell <nandevi001@gmail.com>
2026-07-06 22:34:34 +0530

    Create C_program_12.c

---
+++ C_program_12.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[50];
+    char str[51];
+    int n,i;
+
+    printf("Enter number of digits: ");
+    scanf("%d",&n);
+
+    printf("Enter the digits:\n");
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        str[i]=arr[i]+'0';
+    }
+
+    str[n]='\0';
+
+    printf("String = %s",str);
+}
